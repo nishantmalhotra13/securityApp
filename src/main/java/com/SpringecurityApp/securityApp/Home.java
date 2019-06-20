8f06d721c4d24d28a2ce220ec598fe08d4d04b55
A00430215 <46535160+A00430215@users.noreply.github.com>
2019-06-20 19:12:46 -0300

    added basic security

---
+++ src/main/java/com/SpringecurityApp/securityApp/Home.java
@@ -0,0 +1,13 @@
+package com.SpringecurityApp.securityApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home.jsp";
+    }
+}
